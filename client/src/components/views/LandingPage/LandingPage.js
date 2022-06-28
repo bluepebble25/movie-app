f545e8de1b9f564d2d020034cd744f9396069fef
bluepebble25 <bluepebble25@gmail.com>
2022-06-28 17:19:53 +0900

    feat(LandingPage): add MainImage Component

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,18 +1,33 @@
-import React, { useEffect } from 'react';
-import { API_URL, API_KEY } from '../../../config/Config';
+import React, { useEffect, useState } from 'react';
+import { API_URL, API_KEY, BASE_IMAGE_URL } from '../../../config/Config';
+import MainImage from './Sections/MainImage';
 
 function LandingPage() {
+
+    const [Movies, setMovies] = useState([]);
+    const [MainMovieImage, setMainMovieImage] = useState(null);
 
     useEffect(() => {
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko&page=1`;
 
         fetch(endpoint)
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then(data => {
+                setMovies(data.results);
+                setMainMovieImage(data.results[0]);
+            });
     }, []);
 
     return (
         <div style={{ width: '100%', margin: '0' }}>
+            {/* Main Image */}
+            {MainMovieImage &&
+                <MainImage
+                image={`${BASE_IMAGE_URL}w1280${MainMovieImage.backdrop_path}`}
+                title={`${MainMovieImage.title}`}
+                text={`${MainMovieImage.overview}`}
+            />}
+
             <div style={{ width: '85%', margin: '1rem auto' }}>
                 <h2>Movies by latest</h2>
                 <hr />
--- client/src/config/Config.js
+++ client/src/config/Config.js
@@ -3,7 +3,7 @@ export const USER_SERVER = '/api/users';
 
 // API
 export const API_URL = 'https://api.themoviedb.org/3/';
-export const BASE_IMAGE_URL = 'https:/image.tmdb.org/t/p/';
+export const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/';
 
 // API KEY
 export const API_KEY = process.env.REACT_APP_API_KEY;
