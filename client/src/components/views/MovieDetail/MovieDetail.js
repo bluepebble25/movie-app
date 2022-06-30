98ad4d251ea2a33a625f092426a34dc7dc617268
bluepebble25 <bluepebble25@gmail.com>
2022-06-30 12:49:08 +0900

    feat(MovieDetail): add showing casts feature

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -2,20 +2,37 @@ import React, { useEffect, useState } from 'react'
 import { API_URL, API_KEY, BASE_IMAGE_URL } from '../../../config/Config';
 import MainImage from '../commons/MainImage';
 import MovieInfo from './Section/MovieInfo';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 function MovieDetail(props) {
   let movieId = props.match.params.movieId;
   const [Movie, setMovie] = useState([]);
+  const [Casts, setCasts] = useState([]);
+  const [ActorToggle, setActorToggle] = useState(false);
 
   useEffect(() => {
     const endpointInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=ko`;
+    const endpointCrews = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}&language=ko`;
+
     fetch(endpointInfo)
       .then(response => response.json())
       .then(data => {
-        console.log(data);
+        console.log('movie', data);
         setMovie(data);
       });
+
+    fetch(endpointCrews)
+    .then(response => response.json())
+    .then(data => {
+      console.log('crew', data);
+      setCasts(data.cast);
+    });
   }, [movieId]);
+
+  const toggleActorView = () => {
+    setActorToggle(!ActorToggle);
+  }
 
   return (
     <div>
@@ -27,13 +44,32 @@ function MovieDetail(props) {
       />
       {/* Body */}
       <div style={{ width: '85%', margin: '1rem auto' }}>
+
         {/* Movie Info */}
+
         <MovieInfo movie={Movie}/>
         <br />
+
         {/* Actors Info */}
+
         <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
-          <button>Toggle Actor View</button>
+          <button onClick={toggleActorView}>Toggle Actor View</button>
         </div>
+
+        {ActorToggle &&
+        <Row gutter={[16, 16]} >
+          {Casts && Casts.map((cast, index) => (
+            <React.Fragment key={index}>
+              <GridCards
+                image={cast.profile_path ?
+                  `${BASE_IMAGE_URL}w500${cast.profile_path}` : null}
+                castName={cast.name}
+              />
+            </React.Fragment>
+          ))}
+        </Row>
+        }
+        
       </div>
     </div>
   )
